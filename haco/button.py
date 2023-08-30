ab4540aff419747bcfcc7b5822f2ff49b0250fa4
Edward Brown <ed@frontmatter.ai>
2023-08-30 15:17:25 +0100

    Implement Button control

---
+++ haco/button.py
@@ -0,0 +1,21 @@
+from haco import schema
+from haco.control import Control
+
+
+class Button(Control):
+    NAME = 'button'
+
+    @classmethod
+    def get_schema(cls):
+        return schema.Schema(
+            capabilities=[
+                schema.Capability(
+                    name='state',
+                    ha=schema.HomeAssistant(announce_data=schema.AnnounceTopic(key='{io_ha}_topic')),
+                    tamota=None
+                )
+            ]
+        )
+
+    def __init__(self, name, icon=None):
+        super().__init__(name, icon=icon)
