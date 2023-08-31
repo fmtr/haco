eabc1d96c3e80b0ba8ddc92e4990f2a7980582c5
Edward Brown <ed@frontmatter.ai>
2023-08-31 15:20:33 +0100

    Add restart button to demo config

--- haco/configs/demo.py
+++ haco/configs/demo.py
@@ -1,3 +1,13 @@
+from haco import Button, Tasmota
+
+restart = Button('Restart Tasmota', icon='restart')
+
+
+@restart.callback()
+def ha(value) -> Tasmota[str, 'tasmota.cmd("restart 1")']:
+    return value
+
+
 from haco import Sensor
 
 memory = Sensor('Memory Free', icon='memory', uom=Sensor.UOM.DATA_KILOBYTES, device_class=Sensor.DEVICE_CLASS.DATA_SIZE)
